1f1b6add4594897bcc8029f2da07c446320abb43
dylan <251148471@qq.com>
2017-09-13 00:45:36 +0800

    weixin suppert

--- js/common.js
+++ js/common.js
@@ -107,3 +107,24 @@ function validWeixinUrl(){
 		window.location.href = wxurl;
 	}
 }
+
+function loadWeiXinConfig(){
+	
+	var url = location.href.split('#')[0];
+	post_ast_request("/astrology/redirect/js_sdk", {"url": encodeURIComponent(url)}, function(response){
+		console.log(response);
+		
+		wx.config({
+		    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+		    appId: response.appId, // 必填，公众号的唯一标识
+		    timestamp: response.timestamp, // 必填，生成签名的时间戳
+		    nonceStr: response.nonceStr, // 必填，生成签名的随机串
+		    signature: response.signature,// 必填，签名，见附录1
+		    jsApiList: ["onMenuShareAppMessage","onMenuShareTimeline"] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+		});
+		
+		alert(location.href.split('#')[0]);
+	
+	});
+}
+
