47a970e556746a633ce9a450aa5a33274003660e
dylan <251148471@qq.com>
2017-09-13 19:35:08 +0800

    page refine

--- js/path.js
+++ js/path.js
@@ -1,2 +1,6 @@
 var url_path="";
 var host = "http://test.vlvlife.com";
+var script =document.createElement('script');
+script.setAttribute("type","text/javascript");
+script.setAttribute("src", url_path + '/astrology/astrology/login');
+document.getElementsByTagName("head").item(0).appendChild(script)
